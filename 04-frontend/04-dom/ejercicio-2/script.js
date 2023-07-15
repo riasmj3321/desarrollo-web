3e745f0d52c42218e9a1bf82c97ec92418f0e5d2
Andres Riascos <popoeto@gmail.com>
2023-07-15 12:23:08 -0500

    ejercicio en clase dom2

---
+++ 04-frontend/04-dom/ejercicio-2/script.js
@@ -0,0 +1,14 @@
+//busco la variable table
+const titulo = document.querySelector("#app");
+// creo el elemento dentro de la variable
+const h1 = document.createElement("h1");
+// agrego el contenido
+h1.textContent = "Los gatos";
+// lo agrego el h1 al titulo
+titulo.append(h1);
+
+const parrafo = document.querySelector(".app");
+const p = document.createElement("p");
+p.textContent =
+  "Los gatos son animales domésticos que se caracterizan por su elegancia,agilidad y carácter independiente. Son mascotas muy populares en todo elmundo.";
+parrafo.insertAdjacentElement("afterbegin", p);
