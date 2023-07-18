e2d09b8f0b786dcf2821453be21118b70a24ef17
Andres Riascos <popoeto@gmail.com>
2023-07-18 10:54:45 -0500

    avances ejercicio 2

--- 04-frontend/04-dom/ejercicio-2/script.js
+++ 04-frontend/04-dom/ejercicio-2/script.js
@@ -7,8 +7,12 @@ h1.textContent = "Los gatos";
 // lo agrego el h1 al titulo
 titulo.append(h1);
 
-const parrafo = document.querySelector(".app");
+const parrafo = document.querySelector("#app");
 const p = document.createElement("p");
 p.textContent =
   "Los gatos son animales domésticos que se caracterizan por su elegancia,agilidad y carácter independiente. Son mascotas muy populares en todo elmundo.";
-parrafo.insertAdjacentElement("afterbegin", p);
+parrafo.insertAdjacentElement("afterend", p);
+
+
+
+
